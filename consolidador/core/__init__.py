f630df6a21b0daebdb1c7733c3101df380aa9d39
Brayan Melo <brayan.acevedo28@gmail.com>
2026-03-27 08:08:37 -0500

    refactor: update naming conventions in __init__.py to use English terms

--- consolidador/core/__init__.py
+++ consolidador/core/__init__.py
@@ -1,9 +1,9 @@
 # consolidador/core/__init__.py
-from .procesador import procesar_base, COLUMNAS
-from .analizador import (
-    kpis_globales,
-    resumen_por_convenio,
-    pendientes_por_facturador,
-    detalle_pendientes,
-    convenios_disponibles,
+from .processor import procces_base, columns
+from .analyzer import (
+    global_kpis,
+    summary_by_agreement,
+    pending_by_biller,
+    pending_details,
+    available_agreements,
 )
